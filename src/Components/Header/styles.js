9d0fdbcb8832e486685ad39ee4787617579febe8
EzekielUmanmah <umanmah234@gmail.com>
2021-09-02 17:21:12 -0400

    add header

--- src/Components/Header/styles.js
+++ src/Components/Header/styles.js
@@ -1,11 +1,39 @@
 import styled from 'styled-components';
+import { Link } from 'react-router-dom';
 
 import { ReactComponent } from './redditLogo.svg';
 
 export const Container = styled.header`
-
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin: 0 auto;
+    width: 90vw;
+    height: ${(props) => props.theme.size.headerHeight};
 `;
 
 export const Logo = styled(ReactComponent)`
 
+`;
+
+export const NavLink = styled(Link)`
+    font-size: ${(props) => props.theme.font.size.small};
+    text-decoration: none;
+    transition: 0.4s ease;
+    margin-left: 20px;
+    &:visited {
+        color: inherit;
+    }
+    &:hover {
+        color: ${(props) => props.theme.color.midLight};
+        opacity: 1;
+        cursor: pointer;
+    }
+`;
+
+export const NavLinkContainer = styled.nav`
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    position: relative;
 `;
---
+++ src/styles/theme.js
@@ -0,0 +1,22 @@
+export default {
+  font: {
+    main: 'montserrat',
+    title: 'bitter',
+    lineHeight: 1.69,
+    size: {
+      default: '1.5rem',
+      small: '1rem',
+    },
+  },
+  size: {
+    headerHeight: '100px',
+    footerHeight: '100px',
+  },
+  color: {
+    text: '#93918f',
+    dark: '#000000',
+    midDark: '#636363',
+    midLight: '#d5d5d5',
+    light: '#ffffff',
+  },
+};
