4dad98aafb7441948f1a89280cd6310c583337ca
EzekielUmanmah <umanmah234@gmail.com>
2021-09-02 01:21:01 -0400

    initial commit

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+// import { Link } from 'react-router-dom';
+
+import * as S from './styles';
+
+const Header = () => (
+  <S.Container>
+    <S.Logo />
+  </S.Container>
+);
+
+export default Header;
---
+++ src/Components/Header/styles.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+import { ReactComponent } from './redditLogo.svg';
+
+export const Container = styled.header`
+
+`;
+
+export const Logo = styled(ReactComponent)`
+
+`;
